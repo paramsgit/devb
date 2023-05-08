a558a74789f5027291e603a430772ab998eea375
Sbsbsv <sbsbsv002@gmail.com>
2023-05-09 01:07:49 +0530

    imageisshowing but not moving

--- src/files/post.js
+++ src/files/post.js
@@ -1,7 +1,17 @@
 import React from 'react'
-
+import { ParallaxBanner } from 'react-scroll-parallax';
 export const Post = () => {
-  return (
-    <div>Post</div>
-  )
+    return (
+        <>
+        
+        <div >
+        <ParallaxBanner
+          layers={[{ image: 'abc.jpg', speed: -15 }]}
+          className="aspect-[2/1]"
+          style={{width:"600px",height:"400px"}}
+        />
+        </div>
+
+        </>
+      );
 }
