a17d907002c125851bed2752a214f25676ff9f69
Sbsbsv <sbsbsv002@gmail.com>
2023-05-09 00:59:59 +0530

    un

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 import './mycss.css';
 import { Host } from './files/host';
+import { Post } from './files/Post';
+import { ParallaxProvider } from 'react-scroll-parallax';
 function App() {
   return (
   <>
+   <ParallaxProvider>
   <Host/>
+  <Post/>
+  </ParallaxProvider>
   </>
   );
 }
