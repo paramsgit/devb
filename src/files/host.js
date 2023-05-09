26b1e8536f3867ea96b5def38d6282532e365a11
Sbsbsv <sbsbsv002@gmail.com>
2023-05-10 01:55:04 +0530

    smoothscrollwithlinxand_scrollanle_DONE

--- src/files/host.js
+++ src/files/host.js
@@ -1,38 +1,42 @@
-import React,{useRef} from 'react'
-import { LocomotiveScrollProvider } from 'react-locomotive-scroll'
-import { ParallaxBanner } from 'react-scroll-parallax';
-import LocomotiveScroll from 'locomotive-scroll';
-import abc from './static/abc.jpg'
-export const Host = () => {
-  const containerRef = useRef(null)
+// import React,{useRef} from 'react'
+// import { LocomotiveScrollProvider } from 'react-locomotive-scroll'
+// import { ParallaxProvider } from 'react-scroll-parallax';
+// import { ParallaxBanner } from 'react-scroll-parallax';
+// import LocomotiveScroll from 'locomotive-scroll';
+// import abc from './static/abc.jpg'
+// import { Post } from './post';
+// import { Tost } from './tost';
+// export const Host = () => {
+//   const containerRef = useRef(null)
 
-  return (
-    <>
-    <LocomotiveScrollProvider
-  options={
-    {
-      smooth: true,
-    }
-  }>
-    <div className="div" id='dsc' data-scroll-container ref={containerRef}>
-      
-      <div className="outerone" data-scroll-section >
-        <div className="innerone" ></div>
-      </div>
-      <div className="outerone" data-scroll-section>
-      <ParallaxBanner
-      layers={[{ image: 'abc.jpg', speed: -15 }]}
-      className="aspect-[2/1]"
-    />
-      </div>
-      
-      <div className="outerone" data-scroll-section >
-        <div className="innerone" ></div>
-      </div>
-    </div>
-   
-    </LocomotiveScrollProvider>
+//   return (
+//     <>
   
-    </>
-  )
-}
+//     <LocomotiveScrollProvider
+//   options={
+//     {
+//       smooth: true,
+//     }
+//   }>
+//     <div className="div" id='dsc' data-scroll-container ref={containerRef}>
+    
+//     <div className="outerone" data-scroll-section data-scroll-speed="10">
+
+//       <div>
+//         <div className="innerone" ></div></div>
+//       </div>
+//     {/* <div className="outerone" data-scroll-section >
+        
+//       </div> */}
+//       <div className="outerone" data-scroll-section>
+//         <div className="innerone"  ></div>
+//       </div>
+    
+//     </div>
+   
+//     </LocomotiveScrollProvider>
+  
+ 
+//     </>
+//   )
+// }
