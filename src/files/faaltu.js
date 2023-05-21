ba3bcd01d544031f8343db8af71b796d68102013
Sbsbsv <sbsbsv002@gmail.com>
2023-05-21 16:30:58 +0530

    tw21

---
+++ src/files/faaltu.js
@@ -0,0 +1,15 @@
+<div className="div" id='dsc'>
+    
+    <div className="outerone">
+
+      <div>
+        <div className="innerone" ></div></div>
+      </div>
+    <div className="outerone"  >
+        <Post/>
+      </div>
+      <div className="outerone" >
+        <div className="innerone"  ></div>
+      </div>
+    
+    </div>
