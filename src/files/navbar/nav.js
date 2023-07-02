ad9b960689100b0fbf79c174c2e05f320fc38908
Sbsbsv <sbsbsv002@gmail.com>
2023-07-02 17:50:24 +0530

    saving work

--- src/files/navbar/nav.js
+++ src/files/navbar/nav.js
@@ -1,10 +1,31 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 
 export const Nav = () => {
+  useEffect(() => {
+    const handleScroll = () => {
+      
+      const scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+
+    
+ 
+       if (scrollPosition >= 603) {
+        console.log("up than 603",scrollPosition)
+        
+       } else {
+         
+       }
+    };
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    }
+  }, [])
+  
   return (
     <>
     
-<nav className="bg-white border-gray-200 dark:bg-gray-900">
+<nav className="bg-white border-gray-200 dark:bg-gray-900" id='navbar'>
   <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
     <a href="https://flowbite.com/" className="flex items-center">
         <img src="https://flowbite.com/docs/images/logo.svg" className="h-8 mr-3" alt="Flowbite Logo" />
