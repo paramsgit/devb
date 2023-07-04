aea9d7d8b6c91ea6d4934dfb1d66dfd815b7548c
Sbsbsv <sbsbsv002@gmail.com>
2023-07-04 17:09:02 +0530

    adding edu

---
+++ src/files/education/education.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import edu1 from '../static/edu1.jpg'
+import edu2 from '../static/edu2.jpg'
+export const Education = () => {
+  return (
+    <>
+  <div className="educations">
+ 
+  </div>
+    </>
+  )
+}
