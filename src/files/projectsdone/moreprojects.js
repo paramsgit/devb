d509bf1e41e9ce6b258afb1b4a68a331583cb16a
Sbsbsv <sbsbsv002@gmail.com>
2023-07-12 20:59:46 +0530

    resume changed

--- src/files/projectsdone/moreprojects.js
+++ src/files/projectsdone/moreprojects.js
@@ -15,15 +15,22 @@ export const Moreprojects = () => {
 
     <div className="more_projects">
         <div className="mpm1">
+            <a href="https://afsarbande002.pythonanywhere.com/" target='_blank'>
             <img className='mpimg' src={mp1} alt="" />
+            </a>
             <h4 className='mpname'>Neostore</h4>
         </div>
         <div className="mpm1">
+        <a href="https://gsagriworks.netlify.app/" target='_blank'>
             <img className='mpimg' src={mp2} alt="" />
+            </a>
             <h4 className='mpname'>GS Agri works</h4>
         </div>
         <div className="mpm1">
+        <a href="http://paramveersapp.pythonanywhere.com/" target='_blank'>
+
             <img className='mpimg' src={mp3} alt="" />
+            </a>
             <h4 className='mpname'>Auto-Chatbot</h4>
         </div>
     </div>
